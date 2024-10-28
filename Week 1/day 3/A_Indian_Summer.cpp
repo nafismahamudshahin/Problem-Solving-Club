14be19ec5d9281ea8fedb553354354d10b420cb2
nafismahamudshahin <nafismahamudshahin@gmail.com>
2024-10-28 23:39:31 +0600

    solve Indian summer problem

---
+++ Week 1/day 3/A_Indian_Summer.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    set<pair<string,string>>s;
+    while(n--){
+        string a,b;
+        cin>>a>>b;
+        s.insert({a,b});
+    }
+    cout<<s.size();
+    return 0;
+}
