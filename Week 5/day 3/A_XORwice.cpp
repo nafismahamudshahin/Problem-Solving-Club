6892d7951257ca7ad044e501ccf30c2a89012a64
nafismahamudshahin <nafismahamudshahin@gmail.com>
2024-11-26 22:32:33 +0600

    A XOR wice

---
+++ Week 5/day 3/A_XORwice.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define endl '\n'
+int main(){
+    fast;
+    int t;
+    cin>>t;
+    while(t--){
+        ll a,b;
+        cin>>a>>b;
+        cout<<(a^b)<<endl;
+    }
+    return 0;
+}
