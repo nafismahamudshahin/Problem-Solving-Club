24e132f1a63d427e27a0b3f3232ecd3631f7d8df
nafismahamudshahin <nafismahamudshahin@gmail.com>
2024-11-26 22:33:10 +0600

    Dull Operation

---
+++ Week 5/day 3/Dull_Operation.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define endl '\n'
+int main(){
+    fast;
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<n<<" "<<n-1<<endl;
+    }
+}
